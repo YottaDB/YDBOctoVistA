28591b0fdc15e1830bf59b61b6e73d2ce15ff229
Sam Habiel <sam@yottadb.com>
2021-09-22 11:48:52 -0700

    [#11] Make the output of _YDBOCTOVISTAM.m idempotently applyable to Octo
    Before each CREATE TABLE statement, put a DROP TABLE IF EXISTS statement.
    
    Also, some re-formatting of the comments so that the routine format
    complies with the SAC (esp in this case, first line is routine name).
    
    https://www.hardhats.org/tools/SACC_20191101.pdf section 2.2.1.
    
    Also add commit-verify to pipeline, as we never had a pipeline on this
    repo before.

--- _YDBOCTOVISTAM.m
+++ _YDBOCTOVISTAM.m
@@ -1,20 +1,21 @@
-;Copyright (C) 2019 YottaDB, LLC
-;
-; This program is free software: you can redistribute it and/or modify
-; it under the terms of the GNU Affero General Public License as
-; published by the Free Software Foundation, either version 3 of the
-; License, or (at your option) any later version.
-;
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU Affero General Public License for more details.
-;
-; You should have received a copy of the GNU Affero General Public License
-; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-;
-%YDBOCTOVISTAM ; YDB/CJE - Octo-VistA SQL Mapper ;2019-03-20
- ;;1.0;KBBOSQL;**1**;Mar 20, 2019;Build 1
+%YDBOCTOVISTAM ; YDB/CJE/SMH - Octo-VistA SQL Mapper ;2021-09-22
+ ;;1.1;YOTTADB OCTO VISTA UTILITIES;;Sep 22, 2012
+ ;
+ ; Copyright (c) 2019-2021 YottaDB LLC
+ ;
+ ; This program is free software: you can redistribute it and/or modify
+ ; it under the terms of the GNU Affero General Public License as
+ ; published by the Free Software Foundation, either version 3 of the
+ ; License, or (at your option) any later version.
+ ;
+ ; This program is distributed in the hope that it will be useful,
+ ; but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ; GNU Affero General Public License for more details.
+ ;
+ ; You should have received a copy of the GNU Affero General Public License
+ ; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ;
 MAPALL(PATH,VERIFY,DEBUG)
  S DEBUG=$G(DEBUG)
  S VERIFY=$G(VERIFY)
@@ -97,6 +98,9 @@ MAPTABLE(TABLEIEN,SCHEMA,LINE)
  ;
  ; Escape Quotes in the global location
  S TABLEGLOBALLOCATION=$$ESCAPEQUOTES(TABLEGLOBALLOCATION)
+ ;
+ ; DROP Table if it exists
+ S DDL(FILE,LINE)="DROP TABLE IF EXISTS `"_$S(SCHEMA:"SQLI.",1:"")_TABLENAME_"`;",LINE=LINE+1
  ;
  ; Add the CREATE TABLE opening information
  S DDL(FILE,LINE)="CREATE TABLE `"_$S(SCHEMA:"SQLI.",1:"")_TABLENAME_"`(",LINE=LINE+1
---
+++ ci/needs_copyright.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+set -eu
+
+if ! [ $# = 1 ]; then
+	echo "usage: $0 <filename>"
+	exit 2
+fi
+
+file="$1"
+
+# Don't require deleted files to have a copyright
+if ! [ -e "$file" ]; then
+       exit 1
+fi
+
+skipextensions="ref png zwr html ci"	# List of extensions that cannot have copyrights.
+	# .png  -> these are images (i.e. binary files) used in the documentation.
+	#		Same reason as .rst for not requiring a copyright.
+	# .ref  -> reference files used by the test cases (e.g. tests/outref/T0001.ref).
+	#		Those have a fixed format and should not go through copyright changes.
+	# .zwr  -> zwrite format extract file (does not currently allow a comment character).
+	# .html -> there are a couple of files currently under doc/templates which don't need copyrights.
+	# .ci   -> e.g. calltab.ci stores the call-in table which does not currently have a provision for comment characters.
+if echo "$skipextensions" | grep -q -w "$(echo "$file" | awk -F . '{print $NF}')"; then
+	exit 1
+fi
+
+# Determines whether a file should need a copyright by its name
+# Returns 0 if it needs a copyright and 1 otherwise.
+skiplist="COPYING README.md LICENSE .gitignore"
+for skipfile in $skiplist; do
+	if [ "$file" = "$skipfile" ]; then
+		exit 1
+	fi
+done
