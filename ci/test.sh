51cc1356ef0cf43c09613b2f0fbfa0799fb7b8f9
Sam Habiel <sam@yottadb.com>
2021-09-25 16:01:49 -0400

    [#17] Pipeline test job
    This pipeline job is very similar to the Octo test-vista pipeline job.
    
    It uses docker image `yottadb/octo-vehu:latest-master` (which is
    generated nightly by Octo using the latest YottaDB and the latest Octo),
    puts _YDBOCTOVISTAM.m into a routine directory, generate a new vista.sql
    file, use that to run the VistA tests in `test_vista.bats` test suite.
    
    A Dockerfile (which is easily built by using `docker build .` is
    supplied which does exactly the same thing. It provides an easy way to
    test _YDBOCTOVISTAM.m changes.

---
+++ ci/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+# Set verbose mode so we see each command as it gets executed
+set -v
+set -x
+set -u # Enable detection of uninitialized variables.
+set -o pipefail # this way $? is set to zero only if ALL commands in a pipeline succeed. Else only last command determines $?
+set -e # Below ensures any errors in this script cause it to exit with a non-zero status right away
+
+# Copy DDL Generator file over to a directory in $ydb_routines
+cp _YDBOCTOVISTAM.m ~vehu/r/
+
+# Go to instance home
+cd ~vehu
+
+# Source environment
+source ~vehu/etc/env
+
+# Run DDL Generator
+$ydb_dist/yottadb -r %XCMD 'S DUZ=.5,DIQUIET=1,DUZ(0)="@" D DT^DICRW,MAPALL^%YDBOCTOVISTAM("vista.sql")'
+
+# View a little of it for a spot check
+tail -100 vista.sql
+
+# Tune verbosity down (it's waaaay too verbose to see the entire DDL load of the VistA DDL print out)
+sed -i 's/verbosity = "INFO"/verbosity = "ERROR"/' octo.conf
+
+# Install BATS
+pushd /tmp/
+git clone https://github.com/bats-core/bats-core.git
+cd bats-core
+./install.sh /usr/local
+popd
+
+# Add Run Octo VistA Tests
+cd YDBOcto/build
+# Copying the vista.sql below prevents the YDBOcto test code in
+# tests/test_vista.bats.in from downloading the master _YDBOCTOVISTAM and just
+# re-uses the SQL we generated.
+cp ~vehu/vista.sql .
+cmake3 -D TEST_VISTA=ON -D VISTA_ENV_FILE="~vehu/etc/env" ..
+bats -T bats_tests/test_vista.bats
