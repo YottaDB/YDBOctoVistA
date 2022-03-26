YottaDB/YDBOctoVistA

commit a52d81ed305698c416732ebe65de700da5192134

Change translations for date, timestamp and primary_key to numeric
Octo previously treated INTEGER and NUMERIC as equivalent, however
recent changes have changed that equivalency and now the correct
data type needs to be specified.

In VistA dates and timestamps are numerics with a decimal point.
The primary key data type is also converted to a numeric as it is
possible to have decimals in the IEN columns.

_YDBOCTOVISTAM.m
@@ -184,6 +184,7 @@ MAPTABLE(TABLEIEN,SCHEMA,LINE)
  . . ; Global
  . . I COLUMNTYPE'="P" D
  . . . S COLUMNGLOBAL=$$ESCAPEQUOTES($E($G(^DMSQ("C",COLUMNIEN,1)),1,$G(^DD("STRING_LIMIT"),245)))
+ . . . S DDL(FILE,LINE)=DDL(FILE,LINE)_" GLOBAL "_QUOTE_TABLEOPENGLOBAL_COLUMNGLOBAL_QUOTE
  . . . ;
  . . . ; Piece/$EXTRACT
  . . . S PIECE=""
@@ -191,7 +192,6 @@ MAPTABLE(TABLEIEN,SCHEMA,LINE)
  . . . S EXTRACTSTART=$P(^DMSQ("C",COLUMNIEN,0),U,12)
  . . . S EXTRACTEND=$P(^DMSQ("C",COLUMNIEN,0),U,13)
  . . . I $G(PIECE) D
- . . . . S DDL(FILE,LINE)=DDL(FILE,LINE)_" GLOBAL "_QUOTE_TABLEOPENGLOBAL_COLUMNGLOBAL_QUOTE
  . . . . S DDL(FILE,LINE)=DDL(FILE,LINE)_" PIECE "_PIECE
  . . . I $G(EXTRACTSTART) D
  . . . . S DDL(FILE,LINE)=DDL(FILE,LINE)_" EXTRACT ""$E($G("_TABLEOPENGLOBAL_COLUMNGLOBAL_"),"_EXTRACTSTART_","_EXTRACTEND_")"""
@@ -257,9 +257,9 @@ GETTYPE(ELEMENTIEN,COLUMNIEN)
  ; Moment and memo aren't Standard SQL types
  S COLUMNSQLTYPE=$S(COLUMNSQLTYPE="MOMENT":"DATE",COLUMNSQLTYPE="MEMO":"TEXT",1:COLUMNSQLTYPE)
  ; TODO: PRIMARY_KEY and DATE aren't valid either
- S COLUMNSQLTYPE=$S(COLUMNSQLTYPE="DATE":"INTEGER",COLUMNSQLTYPE="PRIMARY_KEY":"INTEGER",1:COLUMNSQLTYPE)
+ S COLUMNSQLTYPE=$S(COLUMNSQLTYPE="DATE":"NUMERIC",COLUMNSQLTYPE="PRIMARY_KEY":"NUMERIC",1:COLUMNSQLTYPE)
  ; TODO: TIMESTAMP and TEXT aren't valid either
- S COLUMNSQLTYPE=$S(COLUMNSQLTYPE="TIMESTAMP":"INTEGER",COLUMNSQLTYPE="TEXT":"VARCHAR("_$G(^DD("STRING_LIMIT"),245)_")",1:COLUMNSQLTYPE)
+ S COLUMNSQLTYPE=$S(COLUMNSQLTYPE="TIMESTAMP":"NUMERIC",COLUMNSQLTYPE="TEXT":"VARCHAR("_$G(^DD("STRING_LIMIT"),245)_")",1:COLUMNSQLTYPE)
  ; Get the default width of the Column for CHARACTER data types
  I COLUMNSQLTYPE="CHARACTER" D
  . S LENGTH=$P(^DMSQ("C",COLUMNIEN,0),U,2)

commit fee452c94e06465438b46adeccbfc22a2a9af87b

[#25] POINTER fields are inconsistent, and cause problems with some clients
As reported by an end user, it seems that there is a bug due to the
inconsistent data types for POINTERS in the DDL mapper:

Pointers produce "INTEGER", as in

```
`PATIENT` INTEGER GLOBAL "^BPST(keys(""BPS_TRANSACTION_ID""),0)" PIECE 6
```

But the definition of the field is:

```
`PATIENT_ID` NUMERIC PRIMARY KEY START 0 ENDPOINT '$CHAR(0)'
```

Pointers needs to be `NUMERIC` as well.

This seems to be a regression in commit a52d81ed305698c416732ebe65de700da5192134.

Upon investigation, I found that SQLI has a bug where it says that all
Pointers are Integers, which is not correct. NEW PERSON has .5 and .6
IENs, and about 10 files have a .001 of a Date, which may have a decimal
point. See 3^DMSQD and 13^DMSQD for the incorrect SQLI code.

Right now, we don't plan on trying to maintain the SQLI package, so we will
work around it by hardcoding the type NUMERIC for Pointers.

_YDBOCTOVISTAM.m
@@ -1,7 +1,7 @@
-%YDBOCTOVISTAM ; YDB/CJE/SMH - Octo-VistA SQL Mapper ;2021-09-22
- ;;1.2;YOTTADB OCTO VISTA UTILITIES;;Sep 22, 2012
+%YDBOCTOVISTAM ; YDB/CJE/SMH - Octo-VistA SQL Mapper ;2022-03-25
+ ;;1.3;YOTTADB OCTO VISTA UTILITIES;;Sep 22, 2012
  ;
- ; Copyright (c) 2019-2021 YottaDB LLC
+ ; Copyright (c) 2019-2022 YottaDB LLC
  ;
  ; This program is free software: you can redistribute it and/or modify
  ; it under the terms of the GNU Affero General Public License as
@@ -264,7 +264,20 @@ GETTYPE(ELEMENTIEN,COLUMNIEN)
  I FMFILE,FMFIELD,$P(^DD(FMFILE,FMFIELD,0),U,2)["Cm" Q "VARCHAR"
  ;
  ; Get the SQL Data type (CHARACTER, INTEGER, ETC)
- S COLUMNSQLTYPE=$P(^DMSQ("DT",$P(^DMSQ("DM",$P(^DMSQ("E",ELEMENTIEN,0),U,2),0),U,2),0),U,1)
+ N DOMAINIEN  S DOMAINIEN=$P(^DMSQ("E",ELEMENTIEN,0),U,2)
+ N DOMAINNAME S DOMAINNAME=$P(^DMSQ("DM",DOMAINIEN,0),U,1)
+ N TYPEIEN    S TYPEIEN=$P(^DMSQ("DM",DOMAINIEN,0),U,2)
+ S COLUMNSQLTYPE=$P(^DMSQ("DT",TYPEIEN,0),U,1)
+ ;
+ ; SQLI has a bug where it says that all Pointers are Integers, which is not
+ ; correct. NEW PERSON has .5 and .6 IENs, and about 10 files have a .001 of a
+ ; Date, which may have a decimal point.
+ ; See 3^DMSQD and 13^DMSQD for the incorrect SQLI code.
+ ;
+ ; Right now, we don't plan on trying to maintain the SQLI package, so we will
+ ; work around it by hardcoding the type NUMERIC for Pointers.
+ I DOMAINNAME="POINTER" Q "NUMERIC"
+ ;
  ; Moment and memo aren't Standard SQL types
  S COLUMNSQLTYPE=$S(COLUMNSQLTYPE="MOMENT":"DATE",COLUMNSQLTYPE="MEMO":"TEXT",1:COLUMNSQLTYPE)
  ; PRIMARY_KEY and DATE aren't valid either